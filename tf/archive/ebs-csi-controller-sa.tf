2966d0baad471f98c8249f59c21c73d2644c593a
Patrick O'Connor <patrickoconnor80@Patricks-MacBook-Pro-2.local>
2024-04-01 23:08:12 -0400

    wip

---
+++ tf/archive/ebs-csi-controller-sa.tf
@@ -0,0 +1,35 @@
+# resource "aws_eks_addon" "this" {
+#   cluster_name = aws_eks_cluster.this.name
+#   addon_name   = "aws-ebs-csi-driver"
+#   service_account_role_arn = aws_iam_role.this.arn
+# }
+
+# resource "aws_iam_role" "this" {
+#   name        = "${local.prefix}-eks-ebs-csi-driver-role"
+#   description = "AWS IAM Role for the Kubernetes service account ebs-csi-controller-sa."
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Principal" : {
+#           "Federated" : aws_iam_openid_connect_provider.this.arn
+#         },
+#         "Action" : "sts:AssumeRoleWithWebIdentity",
+#         "Condition" : {
+#           "StringLike" : {
+#             "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:sub" : "system:serviceaccount:kube-system:ebs-csi-controller-sa",
+#             "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:aud" : "sts.amazonaws.com"
+#           }
+#         }
+#       }
+#     ]
+#   })
+
+#   tags = var.tags
+# }
+
+# resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#   role       = aws_iam_role.this.name
+# }
